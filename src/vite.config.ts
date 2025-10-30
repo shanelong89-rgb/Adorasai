0d04c46bb6c868fb91aaf727a50bd0f01865259d
shanelong89-rgb <shanelong89@gmail.com>
2025-10-30 10:28:11 +0800

    Update vite.config.ts

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 export default defineConfig({
   plugins: [react()],
   root: '.',
-  build: {
+  outDir: 'build',
     outDir: 'dist',
     sourcemap: true,
     emptyOutDir: true,
